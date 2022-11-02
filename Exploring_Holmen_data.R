f568351ae660ebd12a01ea967a484d05b0a8f010
Michelle Nordkvist <michelle.nordkvist@slu.se>
2022-11-02 10:35:51 +0100

    updates!

--- Exploring_Holmen_data.R
+++ Exploring_Holmen_data.R
@@ -21,6 +21,22 @@ val0<-val[which(val$SVol!=0),]
 
 library(ggplot2)
 
+
+#Look at the relationship between damaged volume and damaged area 
+
+ggplot(val)+ 
+  geom_point(aes(x=SVol,y=Areal))+ #Looks like a good correlation, meaning we can use either as response
+  ylim(0,3)+
+  xlim(0,400)+
+  theme_classic()
+
+
+
+
+
+
+
+
 #check site index
 ggplot(data=val)+
   geom_point(aes(x=SI_TM,y=SVol))
@@ -30,7 +46,10 @@ ggplot(data=val)+
   geom_point(aes(x=M3SK_HA_Gr,y=SVol)) #damaged volume
 
 ggplot(data=val)+
-  geom_point(aes(x=M3SK_HA_Gr,y=SVol/M3SK_TOT_G)) #damaged volume per total volume spruce
+  geom_point(aes(x=M3SK_HA_Gr,y=SVol/M3SK_TOT_G))+ #damaged volume per total volume spruce
+  ylim(0,1.5)
+#why are there values of SVol/M3SK_TOT_G that are larger than 1? That should not be possible??
+#the damaged volume should not be larger than the total volume????
 
 ggplot(data=val)+
   geom_point(aes(x=M3SK_HA_Gr,y=Areal)) #damaged area
@@ -61,6 +80,9 @@ ggplot(data=val0)+
 #Need to take number of stands into account since there might be different numbers of stands in diff categories
 #Look at site index as measure of productivity + does it make a difference if it is "pine soils"
 #Variables such as soil moisture, age class, and maybe site index should be ordered categorical vars 
+#make DGV into a above and below 20 cm (i.e. categorical) variable, because we expect no difference above
+#easy to do with ifelse(). 
+#Interactions?
 
 
 #MODELS
@@ -70,6 +92,8 @@ library(car)
 
 model_area<-lm(Areal~M3SK_HA_Gr+M3SK_HA_Lo+ALD_TOT+DGV+MARKF+TR_HA,data=val)
 Anova(model_area)
+summary(model_area)
+
 
 model_vol<-lm(SVol~M3SK_HA_Gr+M3SK_HA_Lo+ALD_TOT+DGV+MARKF+TR_HA,data=val)
 Anova(model_vol)
@@ -82,3 +106,40 @@ Anova(model_vol_p)
 
 ggplot(val)+
   geom_point(aes(x=ALD_TOT,y=Areal))
+
+ggplot(data=val)+
+  geom_point(aes(x=DGV,y=SVol))
+
+
+val$dgv_t<-as.factor(ifelse(val$DGV>20,1,0)) #make binary var for diameter
+val$soil<-as.factor(val$MARKF) #make into factor
+
+val$soil<-factor(val$MARKF,order=TRUE,levels=c("1","2","3","4")) #make soil into ordinal categorical var
+
+model_vol<-lm(SVol~M3SK_HA_Gr+M3SK_HA_Lo+ALD_TOT+TR_HA+dgv_t+soil+tree_site,data=val)
+Anova(model_vol)
+
+#I want to make a column from site index into only G or T
+#Partial string match
+
+library(dplyr)
+library(purrr)
+
+#make site index into character to be able to split 
+val$SI_TM<-as.character(val$SI_TM)
+
+#make a new column based on if G or T in site index column
+val<-val %>%
+  mutate(tree_site = map_chr(SI_TM, ~unlist(strsplit(.,""))[1])) %>%
+  mutate(tree_site = case_when(tree_site == "T" ~ "Pine",
+                            tree_site == "G" ~ "Spruce"))
+
+#make site index and tree site columns into factors 
+val$SI_TM<-as.factor(val$SI_TM)
+val$tree_site<-as.factor(val$tree_site)
+
+ggplot(data=val)+
+  geom_point(aes(x=tree_site,y=SVol/M3SK_TOT_G))+
+  theme_classic()+
+  facet_wrap(~soil)
+
