d84a57e6ad0dd088860bf2118c4655a9f8473f25
Michelle Nordkvist <michelle.nordkvist@slu.se>
2022-11-04 07:52:27 +0100

    m

--- Exploring_Holmen_data.R
+++ Exploring_Holmen_data.R
@@ -8,23 +8,58 @@ str(val)
 val$damage<-ifelse(val$SVol==0,0,1)
 
 
+val$dgv_t<-as.factor(ifelse(val$DGV>20,1,0)) #make binary var for diameter
+
+
+val$soil<-factor(val$MARKF,order=TRUE,levels=c("1","2","3","4")) #make soil into ordinal categorical var
+
+
+#I want to make a column from site index into only G or T
+#Partial string match
+
+library(dplyr)
+library(purrr)
+
+#make site index into character to be able to split 
+val$SI_TM<-as.character(val$SI_TM)
+
+
+#make a new column based on if G or T in site index column
+val<-val %>%
+  mutate(tree_site = map_chr(SI_TM, ~unlist(strsplit(.,""))[1])) %>%
+  mutate(tree_site = case_when(tree_site == "T" ~ "Pine",
+                               tree_site == "G" ~ "Spruce"))
+
+val$site_p<-substring(val$SI_TM,2,3)
+
+str(val)
+
+#make site index and tree site columns into factors 
+val$SI_TM<-as.factor(val$SI_TM)
+val$tree_site<-as.factor(val$tree_site)
+val$site_p<-factor(val$site_p,order=TRUE)
+
+
 #Remove stands without spruce
 
 val<-val[which(val$M3SK_TOT_G>0),]
 
-library(dplyr)
 
-val <- val %>% mutate_at(vars(SI_TM,MALKLASS), as.factor)
 
-#data set without zeros 
+val <- val %>% mutate_at(vars(MALKLASS,Status,ALDERSKLAS,BESTNR), as.factor)
+
+#data set without zero damage
 val0<-val[which(val$SVol!=0),]
+
+#data set without stands with no spruce
+valS<-val[which(val$M3SK_TOT_G!=0),]
 
 library(ggplot2)
 
 
 #Look at the relationship between damaged volume and damaged area 
 
-ggplot(val)+ 
+ggplot(valS)+ 
   geom_point(aes(x=SVol,y=Areal))+ #Looks like a good correlation, meaning we can use either as response
   ylim(0,3)+
   xlim(0,400)+
@@ -32,20 +67,16 @@ ggplot(val)+
 
 
 
-
-
-
-
-
 #check site index
-ggplot(data=val)+
-  geom_point(aes(x=SI_TM,y=SVol))
+ggplot(data=valS)+
+  geom_point(aes(x=SI_TM,y=SVol))+
+  theme_classic()
 
 #check spruce volume - all ways possible
-ggplot(data=val)+
+ggplot(data=valS)+
   geom_point(aes(x=M3SK_HA_Gr,y=SVol)) #damaged volume
 
-ggplot(data=val)+
+ggplot(data=valS)+
   geom_point(aes(x=M3SK_HA_Gr,y=SVol/M3SK_TOT_G))+ #damaged volume per total volume spruce
   ylim(0,1.5)
 #why are there values of SVol/M3SK_TOT_G that are larger than 1? That should not be possible??
@@ -70,9 +101,45 @@ ggplot(data=val0)+
 ggplot(data=val0)+
   geom_point(aes(x=M3SK_HA_Gr,y=Areal/TOTALAREAL)) #damaged area per total area
 
+#Could it be valuable to look at individual factors without the cases when they are zero?
+#For example - look at broadleaves when there are broadleaves 
+#Because there are many 0 broadleave stands and they might confound the pattern
+
+#data set without stands with no spruce
+valL<-valS[which(valS$M3SK_TOT_L!=0),]
+
+ggplot(data=valL)+
+  geom_point(aes(x=M3SK_HA_Lo,y=SVol),size=2)+
+  theme_classic()
 
 
+#Correlation between volume spruce and volume e.g. broadleaves? Problem?
 
+cor.test(valS$M3SK_HA_Gr,valS$M3SK_HA_Lo) #-0.07
+cor.test(valS$M3SK_TOT_G,valS$M3SK_TOT_L) #0.24
+
+#NMDS or similar 
+
+#Damage/no damage, volume, area is explained by the combo of stand vars?
+
+vars<-cbind(valS$ALD_TOT,valS$DGV,valS$M3SK_HA_Gr,valS$M3SK_HA_Lo,valS$TR_HA,valS$soil,valS$tree_site)
+
+vars.pca<-prcomp(vars,center=TRUE,scale. = TRUE)
+vars.pca
+plot(vars.pca)
+summary(vars.pca)
+
+SBB_d<-as.factor(valS$damage)
+
+library(ggfortify)
+sbb.pca.plot <- autoplot(vars.pca,
+                          data = vars,
+                          colour = SBB_d)
+
+biplot(vars.pca)
+
+library(devtools)
+install_github("vqv/ggbiplot")
 
 #Ideas:
 #Do all analyses with area and volume and the proportions
@@ -111,36 +178,6 @@ ggplot(data=val)+
   geom_point(aes(x=DGV,y=SVol))
 
 
-val$dgv_t<-as.factor(ifelse(val$DGV>20,1,0)) #make binary var for diameter
-
-
-val$soil<-factor(val$MARKF,order=TRUE,levels=c("1","2","3","4")) #make soil into ordinal categorical var
-
-
-#I want to make a column from site index into only G or T
-#Partial string match
-
-library(dplyr)
-library(purrr)
-
-#make site index into character to be able to split 
-val$SI_TM<-as.character(val$SI_TM)
-
-
-#make a new column based on if G or T in site index column
-val<-val %>%
-  mutate(tree_site = map_chr(SI_TM, ~unlist(strsplit(.,""))[1])) %>%
-  mutate(tree_site = case_when(tree_site == "T" ~ "Pine",
-                            tree_site == "G" ~ "Spruce"))
-
-val$site_p<-substring(val$SI_TM,2,3)
-
-str(val)
-
-#make site index and tree site columns into factors 
-val$SI_TM<-as.factor(val$SI_TM)
-val$tree_site<-as.factor(val$tree_site)
-val$site_p<-factor(val$site_p,order=TRUE)
 
 
 ggplot(data=val)+
